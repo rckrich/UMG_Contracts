fc49c529b19e5a12cce5864d8dfaf405d4bc0ddc
Javier Serrano <javierserranosolis@gmail.com>
2022-03-15 13:20:00 -0600

    Ready for testing contract

--- contracts/UMGContract.sol
+++ contracts/UMGContract.sol
@@ -9,48 +9,32 @@ import "./RandomlyAssigned.sol";
 
 contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 
-
     /*
     * Private Variables
     */
     uint256 private constant NUMBER_OF_RESERVED_UNICORNS = 2;
-    uint256 private constant MAX_SUPPLY = 6;
+    uint256 private constant MAX_SUPPLY = 10;
+	uint256 private constant MAX_MINTS_PER_WALLET = 2;
 
 	/*
 	 * Public Variables
 	 */
-    //Price that the mint will be costing to the consumers
     uint256 public mintPrice = 0.05 ether;
-    //Determines the number of tokens that have been minted
     uint256 public totalSupply;
-    //Determines the maximum amount of tokens that can be minted
-    //uint256 public maxSupply = 10;
-    //Determines the maximum number that a wallet can mint
-    uint256 public maxMintingPerWallet = 2;
-    //Toggle that determines consumers can mint the NFTs
+
     bool public isMintEnabled;
-    //Dictionary-like object that keeps track of the number of mints that each wallet has done
+
     mapping(address => uint256) public mintedWallets;
 
     constructor() payable ERC721('Unicorn Motorcycle Gang', 'UNICORN') RandomlyAssigned(MAX_SUPPLY, NUMBER_OF_RESERVED_UNICORNS){
-        //maxSupply = 10;
-    }
+
+	}
 
     // ======================================================== Owner Functions
 
     function toggleIsMintEnabled() external onlyOwner{
         isMintEnabled = !isMintEnabled;
     }
-
-    function setMaxMintingPerWallet(uint256 _maxMintingPerWallet) external onlyOwner{
-        maxMintingPerWallet = _maxMintingPerWallet;
-    }
-
-/*
-    function setMaxSupply(uint256 _maxSupply) external onlyOwner{
-        maxSupply = _maxSupply;
-    }
-*/
 
     function disbursePayments(
 		address[] memory payees_,
@@ -70,45 +54,45 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 		require(success, 'Transfer failed.');
 	}
 
-    function mint(uint256 num) external payable{
-        //Checks if mint is enabled
+	// ======================================================== External Functions
+
+    function mint(uint256 count) 
+		external 
+		payable
+		ensureAvailabilityFor(count)
+		validateEthPayment(count)
+	{
         require(isMintEnabled, 'minting not enabled');
-        //Checks if num is not null
-        require(num > 0, 'num is 0 or below');
-        //Checks number of mints per NFT
-        require(mintedWallets[msg.sender] + num <= maxMintingPerWallet, 'exceeds max per wallet');
-        //Checks if the value of price that the costumer calling thins function is the same as the nft
-        require(msg.value == mintPrice * num, 'wrong value');
-        //Checks if there's still nft supply
-        //require(maxSupply > totalSupply, 'sold out');
+        require(count > 0, 'num is 0 or below');
+        require(mintedWallets[msg.sender] + count <= MAX_MINTS_PER_WALLET, 'exceeds max per wallet');
         require(MAX_SUPPLY - NUMBER_OF_RESERVED_UNICORNS > totalSupply, 'sold out');
-        //Checks if it exceeds supply
-        //require(maxSupply > totalSupply + num, 'exceeds maximum supply');
-        require(MAX_SUPPLY - NUMBER_OF_RESERVED_UNICORNS >= totalSupply + num, 'exceeds maximum supply');
-        //Checks if the number of nfts to mint are not above the permited treshold
-        require(num <=  maxMintingPerWallet, 'You only can mint a maximum of 10');
+        require(MAX_SUPPLY - NUMBER_OF_RESERVED_UNICORNS >= totalSupply + count, 'exceeds maximum supply');
+        require(count <=  MAX_MINTS_PER_WALLET, 'You only can mint a maximum of 10');
 
-        //Saves number of mints per wallet
-        mintedWallets[msg.sender] += num;
-        //Increases the number of how many mints have been done
-        totalSupply += num;
+        mintedWallets[msg.sender] += count;
+        totalSupply += count;
 
-        /*
-        //Local variable to save gas on the totalSupply change of value
-        uint256 tokenId = totalSupply;
-        //Hanldes the minting of the nft
-        for(uint256 i; i < num; i++){
-            _safeMint( msg.sender, tokenId + i );
-        }
-        */
-
-        //Hanldes the minting of the nft
-        for(uint256 i; i < num; i++){
-            //_safeMint( msg.sender, tokenId + i );
-            uint256 tokenId = nextToken();
-		    //assert(tokenId > NUMBER_OF_RESERVED_UNICORNS && tokenId <= maxSupply);
-            assert(tokenId > NUMBER_OF_RESERVED_UNICORNS && tokenId <= MAX_SUPPLY);
-		    _safeMint(msg.sender, tokenId);
+        for(uint256 i; i < count; i++){
+		    _mintRandomId(msg.sender);
         }
     }
+
+// ======================================================== Internal Functions
+
+	function _mintRandomId(address to) private {
+        uint256 tokenId = nextToken();
+        assert(tokenId > NUMBER_OF_RESERVED_UNICORNS && tokenId <= MAX_SUPPLY);
+        _safeMint(to, tokenId);
+    }
+
+// ======================================================== Modifiers
+
+	modifier validateEthPayment(uint256 count) {
+		require(
+			mintPrice * count == msg.value,
+			'Ether value sent is not correct'
+		);
+		_;
+	}
+
 }
