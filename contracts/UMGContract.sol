c1708e9530db390858157573f221b661310cf344
Javier Serrano <javierserranosolis@gmail.com>
2022-03-16 16:32:13 -0600

    Added coments and ne functions

--- contracts/UMGContract.sol
+++ contracts/UMGContract.sol
@@ -38,12 +38,17 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 
     mapping(address => uint256) public mintedWallets;
 
+	/*
+	 * Constructor
+	 */
     constructor() payable ERC721('Unicorn Motorcycle Gang', 'UNICORN') RandomlyAssigned(MAX_SUPPLY, NUMBER_OF_RESERVED_UNICORNS){
 
 	}
 
     // ======================================================== Owner Functions
 
+	/// Breaks and pauses contract interaction
+	/// @dev modifies the state of the `contractPaused` variable
 	function circuitBreaker() public onlyOwner {
 		if (contractPaused == false) { 
 			contractPaused = true; 
@@ -52,30 +57,57 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 		}
 	}
 
-	function setMintPrice(uint256 _mintPrice)
+	// Adjust the mint price
+	/// @dev modifies the state of the `mintPrice` variable
+	/// @notice sets the price for minting a token
+	/// @param _newPrice The new price for minting
+	function adjustMintPrice(uint256 _newPrice)
 		external
 		onlyOwner
 		checkIfPaused()
 	{
-		mintPrice = _mintPrice;
+		mintPrice = _newPrice;
 	}
 
+	/// Activate or deactivate minting
+	/// @dev set the state of `isMintEnabled` variable to true or false
+	/// @notice Activate or deactivate the minting event
     function toggleIsMintEnabled() external onlyOwner{
         isMintEnabled = !isMintEnabled;
     }
 
-	function addAddresToWhiteList(address[] memory _whiteList)
+	/// Advance Phase
+	/// @dev Advance the sale phase state
+	/// @notice Advances sale phase state incrementally
+	function enterPhase(SalePhase _phase) external onlyOwner {
+		require(uint8(_phase) != uint8(phase), 'Can only change phases');
+		phase = _phase;
+	}
+
+	/// Adds addresses to white list
+	/// @dev Adds addresses to an internal list
+	/// @notice Adds a number of addresses to an internal white list.
+	/// @param _addedAddressesList The new addresses that wants to be added to the white list
+	function addAddressesToWhiteList(address[] memory _addedAddressesList)
 		external
 		onlyOwner
 		checkIfPaused()
 	{
-		for (uint256 i; i < _whiteList.length; i++) {
-			whiteList[whiteListCounter] = _whiteList[i];
+		for (uint256 i; i < _addedAddressesList.length; i++) {
+			whiteList[whiteListCounter] = _addedAddressesList[i];
 			whiteListCounter++;
 		}
 	}
 
-	function claimReservedTokens(address to, uint256[] memory tokensId) 
+	/// Adds an array of tokens to an address
+	/// @dev Adds reserved tokens to an address that has been reserved
+	/// @notice Adds reserved tokens to an address
+	/// @param to recipient address
+	/// @param tokensId The reserved tokens that will be added to the address
+	function claimReservedTokens(
+		address to,
+		uint256[] memory tokensId
+	) 
 		external 
 		onlyOwner 
 		ensureAvailabilityFor(tokensId.length)
@@ -96,6 +128,10 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 		}
 	}
 
+	/// Disburse payments
+	/// @dev transfers amounts that correspond to addresses passeed in as args
+	/// @param payees_ recipient addresses
+	/// @param amounts_ amount to payout to address with corresponding index in the `payees_` array
     function disbursePayments(
 		address[] memory payees_,
 		uint256[] memory amounts_
@@ -113,6 +149,8 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 		}
 	}
 
+	/// Make a payment
+	/// @dev internal function called by `disbursePayments` to send Ether to an address
     function makePaymentTo(address address_, uint256 amt_) private {
 		(bool success, ) = address_.call{value: amt_}('');
 		require(success, 'Transfer failed.');
@@ -120,7 +158,11 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 
 	// ======================================================== External Functions
 
-	function whiteListMint(uint256 count) 
+	/// Mint during presale
+	/// @dev mints by addresses validated using the internal white list
+	/// @notice mints tokens with randomized token IDs to addresses eligible for presale
+	/// @param count number of tokens to mint in transaction
+	function mintPresale(uint256 count) 
 		external 
 		payable
 		ensureAvailabilityFor(count)
@@ -144,6 +186,10 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
         }
     }
 
+	/// Public minting open to all
+	/// @dev mints tokens during public sale, limited by `MAX_MINTS_PER_WALLET`
+	/// @notice mints tokens with randomized IDs to the sender's address
+	/// @param count number of tokens to mint in transaction
     function mint(uint256 count) 
 		external 
 		payable
@@ -169,12 +215,14 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 
 // ======================================================== Internal Functions
 
+	/// @dev internal check to ensure a ID outside of the collection, doesn't get minted
 	function _mintRandomId(address to) private {
         uint256 tokenId = nextToken();
         assert(tokenId > NUMBER_OF_RESERVED_UNICORNS && tokenId <= MAX_SUPPLY);
         _safeMint(to, tokenId);
     }
 
+	/// @dev internal check to ensure an address is in the white list
 	function _searchInWhiteList(address to) private view returns(bool) {
 		
 		for (uint256 i; i < whiteList.length; i++) {
@@ -187,6 +235,9 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 
 // ======================================================== Modifiers
 
+	/// Modifier to validate Eth payments on payable functions
+	/// @dev compares the product of the state variable `mintPrice` and supplied `count` to msg.value
+	/// @param count factor to multiply by
 	modifier validateEthPayment(uint256 count) {
 		require(
 			mintPrice * count == msg.value,
@@ -195,6 +246,8 @@ contract UMGContract is ERC721, Ownable, RandomlyAssigned {
 		_;
 	}
 
+	/// Modifier to validate that the contract is not puased
+	/// @dev compares state of the variable `contractPaused` to ensure it is false
 	modifier checkIfPaused() {
 		require(contractPaused == false);
 		_;
