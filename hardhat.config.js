a715a93d5ba8b0bf4afde03aa61c52b5d0ac689b
Javier Serrano <javierserranosolis@gmail.com>
2022-04-04 13:04:26 -0500

    Added changes for Rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,10 +11,10 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
    solidity: "0.8.4",
-   defaultNetwork: "ropsten",
+   defaultNetwork: "rinkeby",
    networks: {
       hardhat: {},
-      ropsten: {
+      rinkeby: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
