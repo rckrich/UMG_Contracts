2a06b84bd3ea36ba9de17fc9dee2947bb66829fa
RCK_Javier_Serrano <javierserranosolis@rckgames.com>
2022-06-14 16:55:01 -0500

    Changed hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,10 +11,10 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
    solidity: "0.8.4",
-   defaultNetwork: "rinkeby",
+   defaultNetwork: "mainnet",
    networks: {
       hardhat: {},
-      rinkeby: {
+      mainnet: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
